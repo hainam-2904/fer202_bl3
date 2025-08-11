d0d097436ec042a0d4b230c82c1ea9e3a0fb7e86
hainam-2904 <lenamne123@gmail.com>
2025-08-11 10:17:27 +0700

    add slo1 slot2

---
+++ slot1/slot1-ex1/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+const myName = "Nam";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello react
+        </p>
+        <p>
+          My name is {myName}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Fer202
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
