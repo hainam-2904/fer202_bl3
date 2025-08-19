15f9af285dc7ceec1c5f19d45adc516bb0d99af0
hainam-2904 <lenamne123@gmail.com>
2025-08-19 08:49:32 +0700

    ex4 slot 14

---
+++ slot14/ex4/src/App.js
@@ -0,0 +1,13 @@
+import ValidatedInput from './ValidatedInput';
+import './App.css';
+
+function App() {
+  return (
+    <div className='container mt-5'>
+      <h1>Love React</h1>
+      <ValidatedInput />
+    </div>
+  );
+}
+
+export default App;
